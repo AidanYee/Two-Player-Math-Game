23a1ccea4abc8e2290c9aae05312d41eeb933022
Aidan Yee <aidan.yee@pm.me>
2022-05-30 20:35:46 +0000

    add basic game and player logic

--- game.rb
+++ game.rb
@@ -1,2 +1,16 @@
+class Game
+  attr_accessor :name, :player_1, :player_2
+  def initialize(name)
+    @name = name
+    @player_1 = Player.new('Obi wan')
+    @player_2 = Player.new('Anakin')
+  end
 
 
+  def start
+    puts "New Game Started. Welcome #{@player_1.name} and #{@player_2.name}"
+    pp @player_1
+    pp @player_2
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
-require "./game"
+require "./game.rb"
 require "./player"
 require "./questions"
+
+new_game = Game.new('New Game')
+
+new_game.start
